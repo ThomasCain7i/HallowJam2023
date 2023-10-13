ea68f541b4af7449d29a2cbe2b193da51654161e
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-10-13 23:00:46 +0100

    Pick up fuses & Inventory

---
+++ Assets/Scripts/Fuses/UseFuse.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseFuse : MonoBehaviour
+{
+    private GameObject gameObj;
+    public GameObject handUI;
+
+    [SerializeField] bool fuse1, fuse2, fuse3;
+
+    [SerializeField] Inventory inventory;
+
+
+    private bool inReach;
+
+
+    void Start()
+    {
+        gameObj = this.gameObject;
+
+        handUI.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            handUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            handUI.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            handUI.SetActive(false);
+
+            if(fuse1)
+            {
+                inventory.fuse1 = true;
+            }
+
+            if (fuse2)
+            {
+                inventory.fuse2 = true;
+            }
+
+            if (fuse3)
+            {
+                inventory.fuse3 = true;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}

2520de42f48d8687c1286f1f00e60ce055c51562
Thomas Cain <115077729+ThomasCain7i@users.noreply.github.com>
2023-10-14 00:07:13 +0100

    Fusebox
    Animation
    Collection and displaying
    more

---
+++ Assets/UseFuseBox.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseFuseBox : MonoBehaviour
+{
+    private GameObject gameObj;
+    [SerializeField] GameObject fuseOb1, fuseOb2, fuseOb3;
+    public GameObject handUI;
+
+    [SerializeField] bool fuse1, fuse2, fuse3, doorOpen, fusesFull;
+
+    [SerializeField] Inventory inventory;
+
+    [SerializeField] Light lightBulb;
+
+    [SerializeField] Animator animator;
+
+    private bool inReach;
+
+
+    void Start()
+    {
+        gameObj = this.gameObject;
+
+        handUI.SetActive(false);
+
+        lightBulb.color = Color.red;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && !fusesFull)
+        {
+            inReach = true;
+            handUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            handUI.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            if (doorOpen)
+            {
+                if (inventory.fuse1 == true)
+                {
+                    fuseOb1.SetActive(true);
+                    fuse1 = true;
+                    handUI.SetActive(false);
+                }
+    
+                if (inventory.fuse2 == true)
+                {
+                    fuseOb2.SetActive(true);
+                    fuse2 = true;
+                    handUI.SetActive(false);
+                }
+
+                if (inventory.fuse3 == true)
+                {
+                    fuseOb3.SetActive(true);
+                    fuse3 = true;
+                    handUI.SetActive(false);
+                }
+            }
+
+            if (doorOpen == false)
+            {
+                animator.SetTrigger("OpenFuse");
+                doorOpen = true;
+            }
+        }
+
+        if (fuse1 == true && fuse2 == true && fuse3 == true)
+        {
+            lightBulb.color = Color.green;
+            fusesFull = true;
+        }
+    }
+}
